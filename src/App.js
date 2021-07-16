45da3b85944c34172720e8ec17a0e91fcc8b9f31
Duong Nguyen <nguyentrongduong18091992@gmail.com>
2021-07-16 12:57:16 +0700

    xoa logo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img className="App-logo" alt="logo" />
+        
         <p>
           NGUYỄN TRỌNG DƯƠNG
         </p>
