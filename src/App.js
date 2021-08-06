3275c2baa93357602d81c912896ac65b35e8f037
Duong Nguyen <nguyentrongduong18091992@gmail.com>
2021-08-06 17:07:50 +0700

    122. Fix giao diện Pagination

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
-import productApi from './api/productApi';
 import Header from './components/Header';
 import NotFound from './components/NotFound';
 import AlbumFeature from './features/Album';
@@ -9,18 +8,7 @@ import ProductFeature from './features/Product';
 import TodoFeature from './features/Todo';
 
 function App() {
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const params = {
-        _limit: 10,
-      };
-      const productList = await productApi.getAll(params);
-      // console.log(productList);
-    };
-
-    fetchProducts();
-  }, []);
-
+  
   return (
     <div className="App">
       <Header />
--- src/features/Product/pages/ListPage.jsx
+++ src/features/Product/pages/ListPage.jsx
@@ -1,4 +1,5 @@
 import { Box, Container, Grid, makeStyles, Paper } from '@material-ui/core';
+import { Pagination } from '@material-ui/lab';
 import React, { useEffect, useState } from 'react';
 import productApi from '../../../api/productApi';
 import ProductList from '../components/ProductList';
@@ -12,27 +13,50 @@ const useStyles = makeStyles((theme) => ({
   right: {
     flex: '1 1 0',
   },
+  pagination: {
+    display: 'flex',
+    flexFlow: 'row nowrap',
+    justifyContent: 'center',
+    paddingBottom: '20px',
+  },
 }));
 
 function ListPage(props) {
   const classes = useStyles();
   const [productList, setProductList] = useState([]);
+  const [pagination, setPagination] = useState({
+    limit: 9,
+    page: 1,
+    total: 10,
+  });
   const [loading, setLoading] = useState(true);
+  const [filters, setFilter] = useState({
+    _page: 1,
+    _limit: 9,
+  });
 
   useEffect(() => {
     (async () => {
       try {
-        const { data } = await productApi.getAll({ _page: 1, _limit: 10 });
+        const { data, pagination } = await productApi.getAll(filters);
         setProductList(data);
+        setPagination(pagination);
+        console.log(data, pagination);
       } catch (error) {
         console.log('Falled to fetch product list: ', error);
       }
 
       setLoading(false);
     })();
-  }, []);
+  }, [filters]);
 
-  console.log('product list', productList);
+  const handlePageChange = (e, page) => {
+    setFilter((x) => ({
+      ...x,
+      _page: page,
+    }));
+  };
+  // console.log('product list', productList);
 
   return (
     <Box>
@@ -43,7 +67,15 @@ function ListPage(props) {
           </Grid>
           <Grid item className={classes.right}>
             <Paper elevation={0}>
-              {loading ? <ProductSkeletonList /> : <ProductList data={productList} />}
+              {loading ? <ProductSkeletonList length={9} /> : <ProductList data={productList} />}
+              <Box className={classes.pagination}>
+                <Pagination
+                  color="primary"
+                  count={Math.ceil(pagination.total / pagination.limit)}
+                  page={pagination.page}
+                  onChange={handlePageChange}
+                ></Pagination>
+              </Box>
             </Paper>
           </Grid>
         </Grid>
