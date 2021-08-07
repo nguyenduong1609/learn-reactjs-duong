4196b06abb8b24ea236d9fa5e9d4b5a8e5a19c2e
Duong Nguyen <nguyentrongduong18091992@gmail.com>
2021-08-07 08:44:35 +0700

    locj theo khoangr gias

--- src/features/Product/components/Filters/FilterByPrice.jsx
+++ src/features/Product/components/Filters/FilterByPrice.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
-import { Box, Button, TextField, Typography } from '@material-ui/core';
+import { Box, Button, makeStyles, TextField, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+    boderTop: `1px solid ${theme.palette.grey[300]}`,
+  },
+  range: {
+    display: 'flex',
+    flexFlow: 'row nowrap',
+    alignItems: 'center',
+
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+
+    '& > span': {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+    },
+  },
+}));
 
 FilterByPrice.propTypes = {
   onChange: PropTypes.func,
 };
 
 function FilterByPrice({ onChange }) {
+  const classes = useStyles();
+
   const [values, setValues] = useState({
     salePrice_gte: 0,
     salePrice_lte: 0,
@@ -30,16 +52,16 @@ function FilterByPrice({ onChange }) {
   };
 
   return (
-    <Box>
+    <Box className={classes.root}>
       <Typography variant="subtitle2">KHOẢNG GIÁ</Typography>
 
-      <Box>
+      <Box className={classes.range}>
         <TextField name="salePrice_gte" value={values.salePrice_gte} onChange={handleChange} />
         <span>-</span>
         <TextField name="salePrice_lte" value={values.salePrice_lte} onChange={handleChange} />
       </Box>
 
-      <Button variant="outlined" color="primary" onClick={handleSubmit}>
+      <Button variant="outlined" color="primary" size="small" onClick={handleSubmit}>
         Áp Dụng
       </Button>
     </Box>
