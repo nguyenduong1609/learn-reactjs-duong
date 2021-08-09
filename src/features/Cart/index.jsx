bf2b5573a3b28365fec5489a719ee53e033b7ae2
Duong Nguyen <nguyentrongduong18091992@gmail.com>
2021-08-09 11:57:13 +0700

    hoan thien gio hang

--- src/features/Cart/index.jsx
+++ src/features/Cart/index.jsx
@@ -1,21 +1,35 @@
 import { Box, Container, Grid, makeStyles, Paper, Typography } from '@material-ui/core';
+import { Delete } from '@material-ui/icons';
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { STATIC_HOST, THUMBNAIL_PLACEHOLDER } from '../../constants';
 import { formatPrice } from '../../utils';
+import { removeFromCart } from './cartSlice';
 import { cartTotalSelector } from './selectors';
 
 const useStyles = makeStyles((theme) => ({
-  root: {},
+  root: {
+    alignItems: 'center',
+  },
   left: {
     width: '150px',
     height: '150px',
     padding: theme.spacing(1.5),
     borderRight: `1px solid ${theme.palette.grey[300]}`,
   },
+  name: {
+    width: '150px',
+    fontSize: theme.typography.h5.fontSize,
+    fontWeight: 'bold',
+    padding: theme.spacing(1.5),
+  },
   right: {
     flex: '1 1 0',
     padding: theme.spacing(1.5),
+  },
+  originalPrice: {
+    marginRight: theme.spacing(2),
+    textDecoration: 'line-through',
   },
 }));
 
@@ -23,16 +37,23 @@ function CartFeature(props) {
   const classes = useStyles();
   const cartTotal = useSelector(cartTotalSelector);
   const render = useSelector((state) => state.cart.cartItems);
+  const dispatch = useDispatch();
 
   // const render = (state) => state.cart.cartItems;
   console.log('render: ', render);
+
+  const handleDelete = (id) => {
+    const action = removeFromCart(id);
+    console.log('id: ', id);
+    dispatch(action);
+  };
 
   return (
     <Container>
       {render.map((x, index) => (
         <Box padding={1} key={index}>
           <Paper elevation={0}>
-            <Grid container>
+            <Grid container className={classes.root}>
               <Grid item className={classes.left}>
                 <Box padding={1} minHeight={100}>
                   <img
@@ -47,14 +68,33 @@ function CartFeature(props) {
                 </Box>
               </Grid>
 
+              <Grid item className={classes.name}>
+                <Typography variant="h6"> {x.product.category.name}</Typography>
+              </Grid>
+
               <Grid item className={classes.right}>
-                <Typography variant="body2"> {x.product.category.name}</Typography>
                 <Typography variant="body2">
                   <Box component="span" fontSize="16px" fontWeight="bold" mr={1}>
                     {formatPrice(x.product.salePrice)}
                   </Box>
-                  {x.product.promotionPercent > 0 ? ` -${x.product.promotionPercent}%` : ''}
+
+                  {x.product.promotionPercent > 0 && (
+                    <>
+                      <Box component="span" className={classes.originalPrice}>
+                        {formatPrice(x.product.originalPrice)}
+                      </Box>
+
+                      <Box component="span">{`-${x.product.promotionPercent}%`}</Box>
+                    </>
+                  )}
                 </Typography>
+              </Grid>
+              <Grid item className={classes.name}>
+                <Typography variant="h6">Số Lượng: {x.quantity}</Typography>
+              </Grid>
+
+              <Grid item className={classes.name}>
+                <Delete onClick={() => handleDelete(x.id)} />
               </Grid>
             </Grid>
           </Paper>
